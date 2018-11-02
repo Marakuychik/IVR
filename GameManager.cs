b8681a7d93c099a37d8bafb8adea4a60195a76bf
Marakuychik <36333780+Marakuychik@users.noreply.github.com>
2018-11-02 23:35:48 +0300

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;	
+using UnityEngine.UI;	
+namespace Completed
+{					
+	public class GameManager : MonoBehaviour
+	{
+		public float lsd = 2f;						
+		public float td = 0.1f;							
+		public int playerhp = 100;						
+		public static GameManager instance = null;				
+		[HideInInspector] public bool playersTurn = true;		
+		private Text lvlText;									
+		private GameObject lvlImage;						
+		private BoardManager boardScript;						
+		private int lvl = 1;								
+		private List<Enemy> enemies;							
+		private bool enemiesMoving;								
+		private bool doingSetup = true;									
+
+		void Awake()
+		{
+            if (instance == null) instance = this;
+			else if (instance != this) Destroy(gameObject);	
+
+			DontDestroyOnLoad(gameObject);
+			enemies = new List<Enemy>();
+			boardScript = GetComponent<BoardManager>();
+			InitGame();
+		}
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static public void CallbackInitialization()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+			
+        static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            instance.lvl++;
+            instance.InitGame();
+        }
+			
+		void InitGame()
+		{
+			doingSetup = true;
+			lvlImage = GameObject.Find("LevelImage");
+			lvlText = GameObject.Find("LevelText").GetComponent<Text>();
+			lvlText.text = "Floor " + lvl;
+			lvlImage.SetActive(true);
+			Invoke("HideLevelImage", lsd);
+			enemies.Clear();
+			boardScript.SetupScene(lvl);
+			
+		}
+
+		void HideLevelImage()
+		{
+			lvlImage.SetActive(false);
+			doingSetup = false;
+		}
+
+		void Update()
+		{
+			if(playersTurn || enemiesMoving || doingSetup) return;
+
+			StartCoroutine (MoveEnemies ());
+		}
+
+		public void AddEnemyToList(Enemy script)
+		{
+			enemies.Add(script);
+		}
+
+		public void GameOver()
+		{
+			lvlText.text = "On " + lvl + " floor you were killed";
+			lvlImage.SetActive(true);
+			enabled = false;
+		}
+
+		IEnumerator MoveEnemies()
+		{
+			enemiesMoving = true;
+			yield return new WaitForSeconds(td);
+
+			if (enemies.Count == 0) 
+			{
+				yield return new WaitForSeconds(td);
+			}
+
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].MoveEnemy ();
+				yield return new WaitForSeconds(enemies[i].moveTime);
+			}
+
+			playersTurn = true;
+			enemiesMoving = false;
+		}
+	}
+}
+
---
+++ Player.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;	
+using UnityEngine.SceneManagement;
+namespace Completed
+{
+	public class Player : MovingObject
+	{
+		public float restartLevelDelay = 1f;		
+		public int ppp = 10;				
+		public int wallDamage = 1;					
+		public Text foodText;													
+		private int food;                           
+
+		protected override void Start ()
+		{
+			food = GameManager.instance.playerhp;
+			foodText.text = "HP: " + food;
+			base.Start ();
+		}
+
+		private void OnDisable ()
+		{
+			GameManager.instance.playerhp = food;
+		}
+				
+		private void Update ()
+		{
+			if(!GameManager.instance.playersTurn) return;	
+
+			int horizontal = 0;  	
+			int vertical = 0;		
+			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+			vertical = (int) (Input.GetAxisRaw ("Vertical"));
+
+			if(horizontal != 0)
+			{
+				vertical = 0;
+			}
+				
+			if(horizontal != 0 || vertical != 0)
+			{
+				AttemptMove<Wall> (horizontal, vertical);
+			}
+		}
+
+		protected override void AttemptMove <T> (int xDir, int yDir)
+		{
+			foodText.text = "HP: " + food;
+			base.AttemptMove <T> (xDir, yDir);
+			RaycastHit2D hit;
+			CheckIfGameOver ();
+			GameManager.instance.playersTurn = false;
+		}
+	
+		protected override void OnCantMove <T> (T component)
+		{
+			Wall hitWall = component as Wall;
+			hitWall.DamageWall (wallDamage);
+		}
+
+		private void OnTriggerEnter2D (Collider2D other)
+		{
+			if(other.tag == "Exit")
+			{
+				Invoke ("Restart", restartLevelDelay);
+				enabled = false;
+			}
+			else if(other.tag == "Food")
+			{
+				food += ppp;
+				foodText.text = "+" + ppp + " HP: " + food;
+				other.gameObject.SetActive (false);
+			}
+		}
+
+		private void Restart ()
+		{
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+		}
+
+		public void LoseFood (int loss)
+		{
+			food -= loss;
+			foodText.text = "-"+ loss + " HP: " + food;
+			CheckIfGameOver ();
+		}
+
+		private void CheckIfGameOver ()
+		{
+			if (food <= 0) 
+			{
+				GameManager.instance.GameOver ();
+			}
+		}
+	}
+}
+
